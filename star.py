4a35c39ee11b3d48742b69bc36eebe59c25f9d3e
Harikesh-MAurya <113936189+Harikesh-MAurya@users.noreply.github.com>
2022-09-20 00:22:36 +0530

    Add files via upload

---
+++ star.py
@@ -0,0 +1,33 @@
+# for i in range(0,5):
+#     for j in range(0,5):
+
+#         print("*",end=" ")
+#     print("\n")
+
+# .........................
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if i<=j:
+#             print("*",end=" ")
+#     print("\n")
+
+# .........................
+
+# i=0
+# a=0
+# b=1
+# c=0
+# while i<=20:
+#     c=a+b
+#     print(c,end=" ")
+#     a=b
+#     b=c
+#     i+=1
+
+# ....................
+
+a=(1,2,3,4,5,7,8,9)
+b=a+(11,22,33,44)
+print(b)
+
